399bf8e7f818d2f1caf242988a4a8ec4611574a7
HE YJ <heyijia_2013@163.com>
2015-08-22 22:18:04 +0800

    using g2o as a lib

---
+++ g2o_test/build/CMakeFiles/g2o_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/g2o_test.dir/main.cpp.o"
+  "../bin/g2o_test.pdb"
+  "../bin/g2o_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/g2o_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
